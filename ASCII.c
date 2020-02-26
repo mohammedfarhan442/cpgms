e9a1217ea4195f5304f7f569a65d1b526ce7cdb6
mohammedfarhan442 <61489694+mohammedfarhan442@users.noreply.github.com>
2020-02-26 12:26:04 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+ {
+ char Character;
+ printf("Enter a Character:");
+ scanf("%c", &Character);
+ 
+printf("ASCII value of Character: %d \n", Character);
+ return 0;
+ }
+
