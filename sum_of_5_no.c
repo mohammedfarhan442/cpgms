d9ebf0063ad8673365f7cda8504a2838696e7723
mohammedfarhan442 <61489694+mohammedfarhan442@users.noreply.github.com>
2020-02-27 15:37:21 +0530

    Add files via upload

---
+++ sum_of_5_no.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main()
+{
+  int a, b, c, d, e, sum;
+ printf("Enter the Five numbers:");
+ scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+ sum = a+b+c+d+e;
+ 
+ printf("Sum of Five numbers:%d \n", sum);
+ return 0;
+ }
