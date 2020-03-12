2385f2926d5d2b54c62731d3a1b7dc55e1d48e0d
mohammedfarhan442 <61489694+mohammedfarhan442@users.noreply.github.com>
2020-03-12 15:48:02 +0530

    nd c

--- Insurance_comp.c
+++ Insurance_comp.c
@@ -1,55 +1,38 @@
 #include<stdio.h>
 int main()
- {
- 
-int Select;
-int age, gender, status, male, female, married, unmarried;
- printf("------------------------------------------------- \n");
- printf("1. Select your current status (Married/ Unmarried): \n");
- printf("2. Select your Gender (M / F): \n");
- printf("3. Input your Age: \n");
- 
- printf("Fill the form: \n");
- scanf("%d", &Select);
- 
- switch (Select);
- 
- {
-  case 1: 
-   
-    if (status=married)
- 
- printf("Applicable for further process. %d \n", status);
- 
+{
+char sex,ms;
+int age;
 
- else 
-  
-  printf("Not Eligible for further process. %d \n", status);
- 
-  break;
+printf ("Enter age: \n");
+scanf("%d", &age);
+printf("Enter Sex: \n");
+scanf("%c", &sex);
+printf("\nEnter MS");
+scanf("%c", &ms);
+if (ms=='M')
 
- case 2:
-    if (gender=male)
- 
- printf("Male %d \n", gender);
- 
- else 
-  
- printf("Female %d \n", gender);
- 
-   break;
- case 3:
- 
-    if (age<=40)
- 
-  printf(" only for male driver. %d \n", age);
- 
- 
- else 
-  if (age<=36)
- 
-printf("female %d \n", age);
+printf ("The driver is insured \n");
+else
+{
+if (sex=='M')
 
- break;
+{
+if (age>30)
+printf ("Driver is insured \n");
+
+else
+printf ("Driver is not insured \n");
+}
+else
+{
+
+if (age>25)
+printf ("Driver is insured \n");
+else
+printf ("Driver is not insured \n");
+
+
+}
 }
 }
